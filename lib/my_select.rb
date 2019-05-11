046b42df27322a909781d0b10f58ea3487c32e80
Evan Schechter <schechter.evan@gmail.com>
2019-05-11 11:12:09 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
- # your code here!
+  if ! block_given?
+    return "Give me a block!"
+  end
+  i = 0
+  newCollect = []
+  while i < collection.length
+    if yield collection[i]
+      newCollect << collection[i]
+    end
+    i += 1
+  end
+  return newCollect
 end
